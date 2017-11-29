49fcffe057d01db8865c6a17bb494417ac776c5c
Epulapp <Epulapp@EPULAPP69.univ-lyon1.fr>
2017-11-29 17:05:30 +0100

    create broadcast receiver

--- app/src/main/java/com/example/epulapp/applicationmobile/GameFragment.java
+++ app/src/main/java/com/example/epulapp/applicationmobile/GameFragment.java
@@ -1,9 +1,13 @@
 package com.example.epulapp.applicationmobile;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -49,6 +53,13 @@ public class GameFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+
     }
 
     @Override
